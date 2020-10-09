d933e9af2f02f4e92b23e5143de35a7df665cda1
unknown <alihaider589@outlook.com>
2020-10-09 07:14:17 -0700

    adding icons and adding safearea view

--- src/pages/Newsletter.js
+++ src/pages/Newsletter.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
-import { Text, View,StyleSheet, StatusBar } from 'react-native'
-
+import { Text, View,StyleSheet, StatusBar, Image,TouchableOpacity ,Dimensions,SafeAreaView} from 'react-native'
+import {WebView} from 'react-native-webview'
+import Header from '../components/Header'
 export default class Newsletter extends Component {
+    constructor(props){
+        super(props)
+    }
     render() {
+        const INJECTEDJAVASCRIPT = `const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `
+        const HEIGHT = Dimensions.get("window").height;
+      
         return (
+            <SafeAreaView
+            style={{
+              width: "100%",
+              backgroundColor: 'white',
+              height: HEIGHT,
+              flex: 1,
+              marginTop: Platform.OS == "android" ? StatusBar.currentHeight : 0,
+            }}
+          >
             <View style={styles.container}>
-                <Text> NewsLetter</Text>
+
+               
+               <WebView
+   
+        source={{ uri: 'https://www.scubadivermag.com/newsletter-subscription/' }}
+        
+      />
             </View>
+            </SafeAreaView>
         )
     }
 }
@@ -15,6 +38,7 @@ export default class Newsletter extends Component {
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        marginTop:StatusBar.currentHeight
+        marginTop:StatusBar.currentHeight,
+        height:Dimensions.get('window').height
     }
 })
